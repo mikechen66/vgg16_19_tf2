a6f296e8597e9694cbdc69dc7d3ee000a4477135
mikechen66 <875661606@qq.com>
2020-09-27 21:37:00 +0800

    Update vgg16_obj_sca.py

--- vgg16_model_sets/vgg16_obj_sca.py
+++ vgg16_model_sets/vgg16_obj_sca.py
@@ -63,18 +63,13 @@ class Vgg16(Sequential):
         self.add(Dense(units=num_classes, activation="softmax"))
 
 
-if __name__ == '__main__':
-
-    IMAGE_WIDTH = 227
-    IMAGE_HEIGHT = 227
-    CHANNELS = 3
-    NUM_CLASSES = 1000
-
+if __name__ == '__main__': 
     # Assign the vlaues 
-    INPUT_SHAPE = (IMAGE_WIDTH, IMAGE_HEIGHT, CHANNELS)
+    input_shape = (227,227,3)
+    num_classes = 1000
 
-    # Call the Vgg16 model 
-    model = Vgg16(INPUT_SHAPE, NUM_CLASSES)
+    # Call the Vgg16  model 
+    model = vgg16(input_shape, num_classes)
 
     # show the full model structure of Vgg16 
     model.summary()
